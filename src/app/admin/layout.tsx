6e1f9cc90cb040156a88b3b913096bc4ca97e86c
sumit <sumit9354800@gmail.com>
2026-09-13 18:57:30 +0530

    testing 4

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,12 @@
+
+export default function SiteLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <main>{children}</main>
+    </>
+  );
+}
---
+++ src/app/admin/users/page.tsx
@@ -0,0 +1,15 @@
+export default function User() {
+  return (
+    <section className="min-h-[60vh] px-6 py-20">
+      <div className="mx-auto max-w-7xl">
+        <h1 className="text-4xl font-bold text-[#c31e3b]">
+          About Us
+        </h1>
+
+        <p className="mt-4 text-slate-600">
+          This page is currently under development.
+        </p>
+      </div>
+    </section>
+  );
+}
